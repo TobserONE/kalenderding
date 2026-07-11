5074f52664361ce303b6e2d0aeb2feea49af1abe
TobserONE <tobias.goettlinger@gmx.de>
2026-07-11 16:51:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mein-tracker-v4";
+const CACHE = "mein-tracker-v5";
 const FILES = ["./", "./index.html", "./manifest.json", "./supabase.js", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
